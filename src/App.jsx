8bd368f6ea5c32d0e9fc455d2257398459cb8462
Araib Khan <AraibKhan93@gmail.com>
2023-08-24 00:55:39 +0500

    Created CityList component and configured eslint.

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         const data = await res.json();
         setCities(data);
       } catch (e) {
-        console.log(e);
+        alert("There was an error loading data...");
       }
       setIsLoading(false);
     };
@@ -38,8 +38,14 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList />} />
-          <Route path="cities" element={<CityList />} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>List of Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,7 +1,19 @@
+import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
 
-function CityList() {
-  return <ul className={styles.cityList}>LIST</ul>;
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return <Message message="Add a city by clicking on the map." />;
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem key={city.id} city={city} />
+      ))}
+    </ul>
+  );
 }
 
 export default CityList;
